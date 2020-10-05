74390e7507c4cc29e8e244d3c453adc296e08c40
tthmoon <tthmoon@yahoo.com>
2020-10-05 21:56:12 +0300

    NEW Комментарий к автотестам.

--- src/main/java/settings/Server.java
+++ src/main/java/settings/Server.java
@@ -1,6 +1,6 @@
 package settings;
 
 public final class Server {
-    public static final String IP = "http://192.168.99.100";
+    public static final String IP = "http://127.0.0.1";
     public static final int PORT = 8091;
 }
